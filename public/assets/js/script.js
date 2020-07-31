85968c26dfd9481dbeee553390fcc27b89edeb3d
fred <synapsefred@hotmail.fr>
2020-07-31 17:05:35 +0200

    git pull fred 15

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -42,3 +42,14 @@ document.addEventListener('DOMContentLoaded', function() {
   $( document ).ready(function(){
     $("select").formSelect();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
