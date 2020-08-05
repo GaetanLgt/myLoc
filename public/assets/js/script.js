dfde0ee9315d0cb86c01567214b40b50178e1e1f
fred <synapsefred@hotmail.fr>
2020-08-05 09:03:54 +0200

    git pull fred 31

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -68,3 +68,14 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.datepicker').datepicker();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
